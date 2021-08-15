1624a2e2a7b2e471e869f1fe4c8dc0fed25ab70e
leo-vieiraa <87448838+leo-vieiraa@users.noreply.github.com>
2021-08-15 20:13:49 -0300

    Banco de Dados - Singleton

--- app/src/main/java/com/example/projeto_001/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/projeto_001/service/repository/GuestRepository.kt
@@ -1,8 +1,26 @@
 package com.example.projeto_001.service.repository
 
+import android.content.Context
 import com.example.projeto_001.service.model.GuestModel
 
-class GuestRepository {
+class GuestRepository private constructor(context: Context) {
+
+    private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
+
+    companion object {
+
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(context: Context) : GuestRepository {
+
+            if(!::repository.isInitialized){
+                repository = GuestRepository(context)
+            }
+            return repository
+
+        }
+
+    }
 
     fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
--- app/src/main/java/com/example/projeto_001/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/projeto_001/viewmodel/GuestFormViewModel.kt
@@ -1,14 +1,17 @@
 package com.example.projeto_001.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.projeto_001.service.model.GuestModel
 import com.example.projeto_001.service.repository.GuestRepository
 
-class GuestFormViewModel : ViewModel() {
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val mGuestRepository: GuestRepository = GuestRepository()
+    private val mContext = application.applicationContext
+    private val mGuestRepository: GuestRepository = GuestRepository.getInstance(mContext)
     private val mSaveGuest = MutableLiveData<Boolean>()
     val saveGuest: LiveData<Boolean> = mSaveGuest
 
